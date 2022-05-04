d09cbdd26c0d1211a58357456e31fdb47b82365d
Marnikilis <diana@cravter.com>
2022-05-04 21:41:12 +0300

    -

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <div className={'wrapper'}>nknkkn</div>
+      vhcvhvcdhvc
+    </div>
+  );
+}
+
+export default App;
