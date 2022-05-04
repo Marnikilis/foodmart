e428d8a8b2f943f8f0ff8b217d92d995e8e60703
Marnikilis <diana@cravter.com>
2022-05-04 23:04:26 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from "./Header/Header";
+import Navbar from "./Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Navbar/>
       <div className={'wrapper'}>nknkkn</div>
       vhcvhvcdhvc
     </div>
