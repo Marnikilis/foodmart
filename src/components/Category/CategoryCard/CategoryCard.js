1398081ed111ef5d15214927a53341151486d6ce
Marnikilis <diana@cravter.com>
2022-05-17 22:35:22 +0300

    add card

--- src/components/Category/CategoryCard/CategoryCard.js
+++ src/components/Category/CategoryCard/CategoryCard.js
@@ -4,12 +4,12 @@ import styles from './CategoryCard.module.scss';
 const CategoryCard = ({img, title}) => {
   return (
     <div className={styles.cardContainer}>
-    <div className={styles.cardContent}>
-      <div>
-        <img src={img} alt='Category'/>
+      <div className={styles.cardContent}>
+        <div>
+          <img src={img} alt='Category'/>
+        </div>
+        <div className={styles.title}>{title}</div>
       </div>
-      <div className={styles.title}>{title}</div>
-    </div>
     </div>
   );
 };
