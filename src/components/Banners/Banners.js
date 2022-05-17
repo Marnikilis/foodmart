42a30c598b001aa81dbacfcb945d80eee575c227
Marnikilis <diana@cravter.com>
2022-05-17 22:29:22 +0300

    change Banners

--- src/components/Banners/Banners.js
+++ src/components/Banners/Banners.js
@@ -7,33 +7,33 @@ import muff from './bannersImg/muff.png';
 import BannerCard from "./BannerCard/BannerCard";
 
 
-const discountProducts=[
-  {img: chocolate, discount:'15', title:'Luxa Dark Chocolate', caption:'Chocolate is only the happiness that you can eat.'},
-  {img: muff, discount:'25', title:'Creamy Muffins', caption:'Very tasty & creamy vanilla flavour creamy muffins.'},
+const discountProducts = [
+  {img: chocolate, discount: '15', title: 'Luxa Dark Chocolate', caption: 'Chocolate is only the happiness that you can eat.'},
+  {img: muff, discount: '25', title: 'Creamy Muffins', caption: 'Very tasty & creamy vanilla flavour creamy muffins.'},
 ];
 
 const Banners = () => {
   return (
     <div>
       <div className={'mainContainer'}>
-       <div className={styles.container}>
-         <img src={leavesTop} className={styles.leavesTop}/>
-         <img src={leavesBottom} className={styles.leavesBottom}/>
-         <div className={styles.cards}>
-         <div className={styles.chocolateCard}>
-             <BannerCard item={discountProducts[0]}/>
-           <div className={styles.imageContainer}>
-             <div className={styles.firstBackgroundImage}/>
-           </div>
-         </div>
-         <div className={styles.muffinsCard}>
-           <BannerCard item={discountProducts[1]}/>
-           <div className={styles.imageContainer}>
-             <div className={styles.secondBackgroundImage}/>
-           </div>
-         </div>
-         </div>
-       </div>
+        <div className={styles.container}>
+          <img src={leavesTop} className={styles.leavesTop} alt='Leaves'/>
+          <img src={leavesBottom} className={styles.leavesBottom} alt='Leaves'/>
+          <div className={styles.cards}>
+            <div className={styles.chocolateCard}>
+              <BannerCard item={discountProducts[0]}/>
+              <div className={styles.imageContainer}>
+                <div className={styles.firstBackgroundImage}/>
+              </div>
+            </div>
+            <div className={styles.muffinsCard}>
+              <BannerCard item={discountProducts[1]}/>
+              <div className={styles.imageContainer}>
+                <div className={styles.secondBackgroundImage}/>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
