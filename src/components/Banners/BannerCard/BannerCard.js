10f85d95ba0fb3b1b0067b9fd035a213df14fa34
Marnikilis <diana@cravter.com>
2022-05-17 22:29:04 +0300

    add bannerCard

--- src/components/Banners/BannerCard/BannerCard.js
+++ src/components/Banners/BannerCard/BannerCard.js
@@ -4,14 +4,14 @@ import styles from './BannerCard.module.scss';
 const BannerCard = ({item}) => {
   return (
     <div className={styles.container}>
-       <div className={styles.textContainer}>
-         <span> Upto {item.discount}% Off</span>
-         <span>{item.title}</span>
-         <span>{item.caption}</span>
-         <button className={`blackBtn ${styles.buyBtn}`}>
-           Shop It
-         </button>
-       </div>
+      <div className={styles.textContainer}>
+        <span> Upto {item.discount}% Off</span>
+        <span>{item.title}</span>
+        <span>{item.caption}</span>
+        <button className={`blackBtn ${styles.buyBtn}`}>
+          Shop It
+        </button>
+      </div>
     </div>
   );
 };
