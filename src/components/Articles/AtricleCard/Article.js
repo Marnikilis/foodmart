bde02bd9a85f7b647750ce710abfb18030a85356
Marnikilis <diana@cravter.com>
2022-05-17 22:27:45 +0300

    add article

--- src/components/Articles/AtricleCard/Article.js
+++ src/components/Articles/AtricleCard/Article.js
@@ -7,16 +7,21 @@ const Article = ({content}) => {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer} style={{backgroundImage: `url(${content.img})`}}/>
-      {/*<img src={content.img} alt={content.title}/>*/}
       <div className={styles.dataContainer}>
-      <div> <Present/> <span>{content.data}</span> </div>
-        <div><Clock/> <span>{content.time}</span></div>
+        <div>
+          <Present/>
+          <span>{content.data}</span>
+        </div>
+        <div>
+          <Clock/>
+          <span>{content.time}</span>
+        </div>
       </div>
       <div className={styles.textContainer}>
-       <span>{content.title}</span>
-      Lorem ipsum dolor sit amet, consectetur
-      adipi elit. Aliquet eleifend viverra enim
-      tincidunt donec quam. A in arcu, hendrerit neque dolor morbi...
+        <span>{content.title}</span>
+        Lorem ipsum dolor sit amet, consectetur
+        adipi elit. Aliquet eleifend viverra enim
+        tincidunt donec quam. A in arcu, hendrerit neque dolor morbi...
       </div>
     </div>
   );
