898fe494c64fb0ec6a40e1df2028956d8916bd4f
Marnikilis <diana@cravter.com>
2022-05-19 21:14:41 +0300

    add interface

--- src/components/Articles/AtricleCard/Article.tsx
+++ src/components/Articles/AtricleCard/Article.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
+//@ts-ignore
 import styles from './Article.module.scss';
 import {ReactComponent as Present} from './atricleSvg/present.svg';
 import {ReactComponent as Clock} from './atricleSvg/clock.svg';
 
+interface ArticleType{
+  img:string;
+  data:string;
+  time:string;
+  title:string;
+}
+
 const Article = ({content}) => {
+  const { img, data, time, title} :ArticleType = content;
+
   return (
     <div className={styles.container}>
-      <div className={styles.imageContainer} style={{backgroundImage: `url(${content.img})`}}/>
+      <div className={styles.imageContainer} style={{backgroundImage: `url(${img})`}}/>
       <div className={styles.dataContainer}>
         <div>
           <Present/>
-          <span>{content.data}</span>
+          <span>{data}</span>
         </div>
         <div>
           <Clock/>
-          <span>{content.time}</span>
+          <span>{time}</span>
         </div>
       </div>
       <div className={styles.textContainer}>
-        <span>{content.title}</span>
+        <span>{title}</span>
         Lorem ipsum dolor sit amet, consectetur
         adipi elit. Aliquet eleifend viverra enim
         tincidunt donec quam. A in arcu, hendrerit neque dolor morbi...
